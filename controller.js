460ed175982ad2ce2b5fb23a3267f8ce7572ebe0
Doug Alderman <agnadcp-d1@yahoo.com>
2015-12-16 17:08:37 -0700

    12/16/15 changes

--- controller.js
+++ controller.js
@@ -2,11 +2,30 @@ var app = angular.module('userProfiles');
 
 app.controller('MainController', function($scope, mainService) {
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
+  $scope.currentPage = 1;
+  $scope.totalPages = 0;
 
+
+  $scope.getUsers = function() {
+    mainService.getUsers($scope.currentPage).then(function(response) {
+      $scope.users = response.data;
+      $scope.totalPages = response.total_pages;
+    })
+  };
 
   $scope.getUsers();
 
+  $scope.previous = function() {
+    if ($scope.currentPage > 1) {
+      $scope.currentPage--;
+      $scope.getUsers();
+    }
+  };
+
+  $scope.next = function() {
+    if ($scope.currentPage < $scope.totalPages) {
+      $scope.currentPage++;
+      $scope.getUsers();
+    }
+  };
 });
